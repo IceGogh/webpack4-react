81dedcc7d624f6f33073e6c1952d8009fee2c40d
tiangao <4535292@qq.com>
2018-04-11 18:14:50 +0800

    04game： pengdin 判断输赢规则

--- react-04-game/index.jsx
+++ react-04-game/index.jsx
@@ -22,53 +22,106 @@ class StatusTitle extends React.Component {
 	}
 }
 
+const WinArr = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+]
+
 class Box extends React.Component {
 	constructor() {
 		super()
 		this.clkin = this.clkin.bind(this)
 		this.state = {
 			role: 'X',
-			statusArr: function() {
-				const Arr = []
-				for(let i = 0; i < 9; i++){
-					Arr.push(i)
-				}
-				return Arr
-			}
+			statusArr: [],
+			XArr: [],
+			OArr: []
+			// 初始 statusArr为空数据, 将赋值函数放到  willmount 周期中赋值
+			// statusArr: function() {
+			// 	const Arr = []
+			// 	for(let i = 0; i < 9; i++){
+			// 		Arr.push(i)
+			// 	}
+			// 	return Arr
+			// }
 		}
 	}
-	
+	componentWillMount() {
+		let arr = (() => {
+			const Arr = []
+			for (let i = 0; i < 9; i++) {
+				Arr.push(i)
+			}
+			return Arr
+		})()
+		this.setState({
+			statusArr: arr
+		})
+
+	}
 	clkin(e) {
 		let ind = e.target.attributes['ind'].value
-		let arr = this.state.statusArr()
-		let role = this.state.role
+		let arr = this.state.statusArr
+			, role = this.state.role
+			, Xarr = this.state.XArr
+			, Oarr = this.state.OArr
 		arr[ind] = role
-		if(role === 'X'){
+
+		// 更新棋盘数组
+		this.setState({
+			statusArr: arr
+		})
+
+
+		//  更新 当前下子方  更新双方已经下子数据
+		if (role === 'X') {
+			Xarr.push(Number(ind))
 			this.setState({
-				role: 'O'
+				role: 'O',
+				XArr: Xarr
 			})
-		}else {
+			if (Xarr.length >= 3) {
+				if (
+					WinArr.some(function (item) {
+						console.log('item', item)
+						console.log('X', Xarr)
+						return item === Xarr
+					})
+				) {
+					console.log(13)
+				} else {
+					console.log(24)
+				}
+			}
+		} else {
+			Oarr.push(ind)
 			this.setState({
-				role: 'X'
+				role: 'X',
+				OArr: Oarr
 			})
 		}
 
-		this.setState({
-			statusArr: []
-		})
+
+
 	}
 
 	render() {
 		return (
 			<div>
-				<StatusTitle curRole={this.state.role}/>
+				<StatusTitle curRole={this.state.role} />
 				<div className="box-Wrap">
 					{
-						this.state.statusArr().map((item, index) => {
-							return <span 
-											key={index}
-											ind={index}
-											onClick={this.clkin}></span>
+						this.state.statusArr.map((item, index) => {
+							return <span
+								key={index}
+								ind={index}
+								onClick={this.clkin}>{item}</span>
 						})
 					}
 				</div>
@@ -79,8 +132,8 @@ class Box extends React.Component {
 
 ReactDOM.render(
 	<div>
-		<Hey/>
-		<Box/>
+		<Hey />
+		<Box />
 	</div>,
 	document.getElementById('hey')
 )
